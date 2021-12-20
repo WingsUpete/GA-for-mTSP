e5c82a273f1071bcda25b0d4e852c2a309511272
Peter S <petershen815@126.com>
2021-12-20 19:47:01 +0800

    [UTIL] Changes the init package naming

--- util/__init__.py
+++ util/__init__.py
@@ -1,2 +1,2 @@
 from .Logger import Logger
-from .Util import route_lengths, plotGD, path2FileNameWithoutExt, extractFinalDistListFromLogDir
+from .Util import route_lengths, plotGD, path2FileNameWithoutExt, extractResListFromLogDir
